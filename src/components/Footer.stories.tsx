a3ee13fd0b2bd4b314f276708599173afc557ad4
Rhys Adey <rhys.adey@diamond.ac.uk>
2024-12-06 11:22:08 +0000

    Adding footer and footer stories

---
+++ src/components/Footer.stories.tsx
@@ -0,0 +1,83 @@
+import { Meta, StoryObj } from "@storybook/react/*";
+import { Footer, FooterLink, FooterLinks } from "./Footer";
+
+const meta: Meta<typeof Footer> = {
+  title: "SciReactUI/Navigation/Footer",
+  component: Footer,
+  decorators: [
+    (Story) => (
+      <div style={{ position: "relative" }}>
+        <div style={{ position: "relative", paddingBottom: "5rem" }}>
+          <Story />
+        </div>
+      </div>
+    ),
+  ],
+  tags: ["autodocs"],
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const LogoOnly: Story = {
+  args: {},
+};
+
+export const CopyrightOnly: Story = {
+  args: {
+    logo: "",
+    copyright: "Copyright © text",
+  },
+};
+
+export const CopyrightAndLogo: Story = {
+  args: { copyright: "Copyright © text" },
+};
+
+export const WithOneLink: Story = {
+  args: {
+    copyright: "Copyright © text",
+    children: [
+      <FooterLinks key="footer-links">
+        <FooterLink href="#" key="first-footer-link">
+          Link one
+        </FooterLink>
+      </FooterLinks>,
+    ],
+  },
+};
+
+export const WithTwoLinks: Story = {
+  args: {
+    copyright: "Copyright © text",
+    children: [
+      <FooterLinks key="footer-links">
+        <FooterLink href="#" key="first-footer-link">
+          Link one
+        </FooterLink>
+        <FooterLink href="#" key="second-footer-link">
+          Link two
+        </FooterLink>
+      </FooterLinks>,
+    ],
+  },
+};
+
+export const WithThreeLinks: Story = {
+  args: {
+    copyright: "Copyright © text",
+    children: [
+      <FooterLinks key="footer-links">
+        <FooterLink href="#" key="first-footer-link">
+          Link one
+        </FooterLink>
+        <FooterLink href="#" key="second-footer-link">
+          Link two
+        </FooterLink>
+        <FooterLink href="#" key="third-footer-link">
+          Link three
+        </FooterLink>
+      </FooterLinks>,
+    ],
+  },
+};
---
+++ src/components/Footer.tsx
@@ -0,0 +1,107 @@
+import { Link, LinkProps, Paper, Typography, useTheme } from "@mui/material";
+import Grid from "@mui/material/Grid2";
+import dlsLogo from "../public/dls.svg";
+
+export interface FooterLinksProps {
+  children: React.ReactElement<LinkProps> | React.ReactElement<LinkProps>[];
+}
+
+export interface FooterProps {
+  /** Location/content of the logo */
+  logo?: string | null;
+  copyright?: string | null;
+  children?: React.ReactElement | React.ReactElement[];
+}
+
+const FooterLinks = ({ children }: FooterLinksProps) => {
+  return (
+    <div
+      style={{
+        alignItems: "center",
+        borderTop: "4px solid transparent",
+        borderBottom: "4px solid transparent",
+      }}
+    >
+      {children}
+    </div>
+  );
+};
+
+const FooterLink = ({ children }: LinkProps) => {
+  const theme = useTheme();
+
+  return (
+    <Link
+      sx={{
+        "&:hover": {
+          color: theme.palette.secondary.main,
+          borderBottom: "solid 4px",
+        },
+        textDecoration: "none",
+        color: theme.palette.primary.contrastText,
+        marginLeft: "1.5rem",
+        cursor: "pointer",
+      }}
+    >
+      {children}
+    </Link>
+  );
+};
+
+/*
+ * Basic footer bar.
+ * Can be used with `FooterLinks` and `FooterLink` to display a list of links.
+ */
+const Footer = ({
+  logo = dlsLogo as string,
+  copyright,
+  children,
+}: FooterProps) => {
+  const theme = useTheme();
+
+  return (
+    <Paper
+      sx={{
+        display: "absolute",
+        bottom: 0,
+        backgroundColor: theme.palette.primary.light,
+        minHeight: 50,
+      }}
+    >
+      <Grid container>
+        <Grid
+          size={{ xs: 6, md: 8 }}
+          style={{
+            alignContent: "center",
+          }}
+        >
+          {children}
+        </Grid>
+        <Grid size={{ xs: 6, md: 4 }}>
+          <div
+            style={{
+              float: "right",
+              paddingTop: "10px",
+              paddingRight: "15px",
+              textAlign: "right",
+            }}
+          >
+            {logo ? <img alt="footer-logo" src={logo} /> : null}
+            {copyright ? (
+              <Typography
+                style={{
+                  margin: 0,
+                  color: theme.palette.primary.contrastText,
+                }}
+              >
+                {copyright}
+              </Typography>
+            ) : null}
+          </div>
+        </Grid>
+      </Grid>
+    </Paper>
+  );
+};
+
+export { Footer, FooterLink, FooterLinks };
