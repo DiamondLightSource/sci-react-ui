a9b26b7285d8dc7048adb9e8b2c8ade25fae0fff
Matthew Wilcoxson <Matthew.Wilcoxson@diamond.ac.uk>
2025-06-26 11:25:12 +0100

    More eslint.

--- src/__test-utils__/helpers.tsx
+++ src/__test-utils__/helpers.tsx
@@ -5,7 +5,8 @@ import { DiamondTheme } from "../themes/DiamondTheme";
 import { render, RenderResult } from "@testing-library/react";
 import { ThemeProviderProps } from "@mui/material/styles/ThemeProvider";
 
-type ThemeProviderPropsWithOptionalTheme = Omit<ThemeProviderProps, "theme"> & Partial<Pick<ThemeProviderProps, "theme">>
+type ThemeProviderPropsWithOptionalTheme = Omit<ThemeProviderProps, "theme"> &
+  Partial<Pick<ThemeProviderProps, "theme">>;
 
 export const renderWithProviders = (
   children: React.ReactNode,
