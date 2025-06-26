33cce185a00a4622deb72a68190a46354f9af35d
Matthew Wilcoxson <Matthew.Wilcoxson@diamond.ac.uk>
2025-06-26 11:22:00 +0100

    Fix type check crybaby.

--- src/__test-utils__/helpers.tsx
+++ src/__test-utils__/helpers.tsx
@@ -1,11 +1,15 @@
+import React from "react";
+
 import { ThemeProvider } from "@mui/material/styles";
 import { DiamondTheme } from "../themes/DiamondTheme";
 import { render, RenderResult } from "@testing-library/react";
 import { ThemeProviderProps } from "@mui/material/styles/ThemeProvider";
 
+type ThemeProviderPropsWithOptionalTheme = Omit<ThemeProviderProps, "theme"> & Partial<Pick<ThemeProviderProps, "theme">>
+
 export const renderWithProviders = (
   children: React.ReactNode,
-  themeOptions?: Omit<ThemeProviderProps, "theme">,
+  themeOptions?: ThemeProviderPropsWithOptionalTheme,
 ): RenderResult => {
   return render(
     <ThemeProvider theme={DiamondTheme} {...themeOptions}>
