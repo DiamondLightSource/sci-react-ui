f31c698930317ebedf0bbb3a7cb763d9bf93e397
Rhys Adey <rhys.adey@diamond.ac.uk>
2025-10-16 12:20:58 +0100

    RA prettier fix

--- src/components/navigation/Breadcrumbs.tsx
+++ src/components/navigation/Breadcrumbs.tsx
@@ -22,7 +22,7 @@ interface BreadcrumbsProps extends BarProps {
  * @param path A single string path, an array of string parts or an array of CustomLink parts
  */
 export function getCrumbs(
-  path: string | string[] | CustomLink[]
+  path: string | string[] | CustomLink[],
 ): CustomLink[] {
   if (typeof path === "string") {
     path = path.split("/");
@@ -60,7 +60,7 @@ const Mui_BreadcrumbsStyled = styled(Mui_Breadcrumbs)<Mui_BreadcrumbsProps>(
   ({ theme }) => ({
     color: theme.vars.palette.primary.contrastText,
     padding: 0,
-  })
+  }),
 );
 
 const Breadcrumbs = ({
