64fff641b7ee0b768d26f8eb102676f643241e29
Matthew Wilcoxson <Matthew.Wilcoxson@diamond.ac.uk>
2025-10-16 10:44:49 +0100

    Half all speeds. And apply "prefers-reduced-motion" to slow way down.

--- src/components/controls/Progress.tsx
+++ src/components/controls/Progress.tsx
@@ -6,15 +6,20 @@ interface ProgressProps {
 }
 
 const speed = {
-  slow: "400ms",
-  medium: "300ms",
-  fast: "200ms",
-};
+	snail : "100000ms",   // imperceptible orbit
+	verySlow : "4000ms",  // 1 orbit per 4 seconds
+	slow: "2000ms",       // 1 orbit per 2 seconds
+	medium: "1000ms",     // 1 orbit per second
+	fast: "500ms",        // 0.5 orbit per second
+	ludicrous: "250ms",   // Sb speed
+	synchrotron: "130ms"  // One earth orbit
+}
 const size = {
-  small: 1,
-  medium: 2,
-  large: 3,
-};
+	small: 1,
+	medium: 2,
+	large: 3,
+	veryLarge: 5
+}
 
 const Progress = (props: ProgressProps) => (
   <Box
@@ -58,6 +63,9 @@ const Progress = (props: ProgressProps) => (
         [`& .${circularProgressClasses.circle}`]: {
           strokeLinecap: "round",
           strokeDasharray: "10 120",
+        },
+        ["@media (prefers-reduced-motion: reduce)"] :{
+          animationDuration: speed["verySlow"],
         },
       })}
       size={35}
