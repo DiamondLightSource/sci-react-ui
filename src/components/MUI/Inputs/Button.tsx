81b7cfc7b59130ca1be8cbf4e0c484db47a9cd8d
VictoriaBeilstenEdmands <victoria.beilsten.edmands@gmail.com>
2025-12-16 14:51:58 +0000

    Add MUI Button story

---
+++ src/components/MUI/Inputs/Button.tsx
@@ -0,0 +1,15 @@
+// src/components/Button/Button.tsx
+import React from "react";
+import MuiButton, { ButtonProps as MuiButtonProps } from "@mui/material/Button";
+
+export type ButtonProps = MuiButtonProps;
+
+export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ children, ...rest }, ref) => (
+    <MuiButton ref={ref} {...rest}>
+      {children}
+    </MuiButton>
+  ),
+);
+
+Button;
