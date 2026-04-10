82be3feae2de7dfe56aecd02e2b7cc0e1d59032e
Guilherme de Freitas <guilherme.de-freitas@diamond.ac.uk>
2026-04-10 16:21:40 +0100

    Upgrade to MUI v7

--- src/themes/DiamondTheme.ts
+++ src/themes/DiamondTheme.ts
@@ -65,6 +65,22 @@ const DiamondThemeOptions = mergeThemeOptions({
     },
   },
   components: {
+    MuiTypography: {
+      defaultProps: {
+        variantMapping: {
+          h1: "h2",
+          h2: "h2",
+          h3: "h2",
+          h4: "h2",
+          h5: "h2",
+          h6: "h2",
+          subtitle1: "h2",
+          subtitle2: "h2",
+          body1: "span",
+          body2: "span",
+        },
+      },
+    },
     MuiButton: {
       styleOverrides: {
         root: ({ theme }: { theme: Theme }) => ({
