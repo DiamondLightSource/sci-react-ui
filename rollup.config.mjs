5f5cdae71d60879d29140ec272af74b67c0abd88
Guilherme de Freitas <guilherme.de-freitas@diamond.ac.uk>
2026-04-13 13:21:31 +0100

    Export controls, fix typos

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -10,7 +10,7 @@ import packageJson from "./package.json" with { type: "json" };
 
 export default [
   {
-    input: ["src/index.ts", "src/navigation.ts", "src/themes.ts"],
+    input: ["src/index.ts", "src/navigation.ts", "src/themes.ts", "src/controls.ts"],
     output: [
       {
         format: "cjs",
