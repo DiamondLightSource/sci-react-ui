0c7f5639d6e4a5fa59b39cd31bb0f23bb3921a9c
Guilherme Francisco <guilherme.de-freitas@diamond.ac.uk>
2026-04-28 09:21:25 +0100

    Upgrade to MUI v7 (#160)
    * Upgrade to MUI v7
    * Update heading variant mapping

--- src/themes/DiamondTheme.ts
+++ src/themes/DiamondTheme.ts
@@ -65,6 +65,22 @@ const DiamondThemeOptions = mergeThemeOptions({
     },
   },
   components: {
+    MuiTypography: {
+      defaultProps: {
+        variantMapping: {
+          h1: "h1",
+          h2: "h2",
+          h3: "h3",
+          h4: "h4",
+          h5: "h5",
+          h6: "h6",
+          subtitle1: "h2",
+          subtitle2: "h3",
+          body1: "span",
+          body2: "span",
+        },
+      },
+    },
     MuiButton: {
       styleOverrides: {
         root: ({ theme }: { theme: Theme }) => ({
