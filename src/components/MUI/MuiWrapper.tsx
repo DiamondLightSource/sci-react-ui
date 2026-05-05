d0f93e30adb8c778440cd02538953bcd37d1f55a
Matthew Wilcoxson <Matthew.Wilcoxson@diamond.ac.uk>
2026-05-05 15:55:57 +0100

    Suggested MUI component wrapper with a few examples.

---
+++ src/components/MUI/MuiWrapper.tsx
@@ -0,0 +1,13 @@
+import { forwardRef } from "react";
+
+export default function MuiWrapper<Prop, Element = HTMLDivElement>(
+  MuiComponent: any,
+  name: string,
+) {
+  const RefForward = forwardRef<Element, Prop>((props, ref) => (
+    <MuiComponent {...props} ref={ref} />
+  ));
+
+  RefForward.displayName = name;
+  return RefForward;
+}
