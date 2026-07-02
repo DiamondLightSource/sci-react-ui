7b3f85b5a03c8fa8fc04d7876fb53cbf4ff0aa7d
Zohar Manor-Abel <zohar.manor-abel@diamond.ac.uk>
2026-07-02 12:09:13 +0100

    Fixed to remove flashes on load and on change of dark/light
    - Updated load based on direction in Installation guide
    - Found that adding  initialisation in HTML helps
    - updated button to reduce flashing on both onLoad and reload

--- dev/src/main.tsx
+++ dev/src/main.tsx
@@ -1,9 +1,13 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
+
+import { InitColorSchemeScript } from "@mui/material";
+
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <InitColorSchemeScript attribute="[data-mode='%s']" />
     <App />
   </React.StrictMode>,
 );
