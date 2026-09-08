7e9d18cde4441c30e6ea815b722f741e2dd00503
Douglas Winter <douglas.winter@diamond.ac.uk>
2026-09-08 11:40:01 +0100

    Export SidebarNav (#287)

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ export * from "./components/navigation/Breadcrumbs";
 export * from "./components/navigation/Footer";
 export * from "./components/navigation/Navbar";
 export * from "./components/navigation/NavMenu";
+export * from "./components/navigation/SidebarNav";
 
 // components/controls
 export * from "./components/controls/AppTitlebar";
